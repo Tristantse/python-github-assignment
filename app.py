244fbac491243f383e15f01c703e7506e2589986
Tristan Tse <tristantse@usf.edu>
2026-04-25 01:29:21 -0400

    Changed calculation to be *5

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# Welcome message
 print("Welcome to my Python program project!")
+# Ask for user input
 hours = input("How many hours did you work today? ")
-hours = float(hours)
-weekly_hours = hours * 7
-print(f"You are on pace to work {weekly_hours} hours this week!")
+# Convert Input and Handle Possible errors
 try:
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number.")
     exit()
+# Perform Calculations
+weekly_hours = hours * 5
+# Display the results
+print(f"You are on pace to work {weekly_hours} hours this week!")
