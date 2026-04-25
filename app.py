d6fbed643d13a7254d933d57385cdf7dd11532df
Tristan Tse <tristantse@usf.edu>
2026-04-25 01:27:51 -0400

    Cleaned up the code, added simple comments and the finalized the program

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 print("Welcome to my Python program project!")
-Hours = Input("How many hours did you work today? ")
-Hours = float(Hours)
-weekly_hours = Hours * 7
+hours = input("How many hours did you work today? ")
+hours = float(hours)
+weekly_hours = hours * 7
 print(f"You are on pace to work {weekly_hours} hours this week!")
 try:
     hours = float(hours)
 except ValueError:
     print("Please enter a valid number.")
     exit()
-    
